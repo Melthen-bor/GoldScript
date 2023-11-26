8ee696e58452a7b421a37420f36365670658f1b7
Melthen-bor <151883748+Melthen-bor@users.noreply.github.com>
2023-11-26 12:55:23 -0800

    Create goldException.java

---
+++ goldException.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+public class goldExeception{
+  public static void goldExeception(String error,String errortype){
+    private final String errormessage = error;
+    private final String type = errortype;
+  }
+  public static String gettype(){
+    return type;
+  }
+  public static String getmessage(){
+    return errormessage;
+  }
+}
