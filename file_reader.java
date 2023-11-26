7a18fc24f20f0fae3165f38e6f057a68cc71618b
Melthen-bor <151883748+Melthen-bor@users.noreply.github.com>
2023-11-26 11:38:22 -0800

    Update file_reader.java

--- file_reader.java
+++ file_reader.java
@@ -2,14 +2,25 @@ import java.util.Scanner;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.nio.file.Files;
 public class file_reader{
   public static void file_reader(String file_name){
     File file = new File(file_name);
   }
-  public static String read(){
-    String out;
-    out = Files.readString(file);
+  public static String[] read(){
+    String[] out;
+    int a = 1;
+    int b = 0;
+    String temp;
+    while(a==1){
+      try{
+        temp = file.nextLine();
+        out[b] = temp;
+        b += 1;
+      } catch(Exception e){
+        a = 0;
+      }
+    }
+    return out;
   }
   public static void change(String file_name){
     file.close();
