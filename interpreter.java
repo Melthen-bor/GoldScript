f50531f9661a5924bfe1e3e42850545c4c6cb54b
Melthen-bor <151883748+Melthen-bor@users.noreply.github.com>
2023-11-26 11:32:25 -0800

    Create interpreter.java

---
+++ interpreter.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+public class interpreter{
+  public static String concate(Object o0,Object o1){
+    return o0+""+o1;
+  }
+  public static void main(String[] args){
+    
+  }
+}
