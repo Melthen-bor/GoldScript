8e6b77dd40d546587dae1d78c4caf28aef6de44e
Melthen-bor <151883748+Melthen-bor@users.noreply.github.com>
2023-11-26 13:10:55 -0800

    Update goldException.java

--- goldException.java
+++ goldException.java
@@ -3,8 +3,11 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 public class goldExeception{
+  public static String concate(Object o0,Object o1){
+    return o0+""+o1;
+  }
   public static void goldExeception(String error,String errortype){
-    private final String errormessage = error;
+    private final String errormessage = concate("\033[31m",conacte(error,"\033[0m")));
     private final String type = errortype;
   }
   public static String gettype(){
