4e3d81ebf1903b22e37b42c19745f4e239db777f
Melthen-bor <151883748+Melthen-bor@users.noreply.github.com>
2023-11-26 16:17:15 -0800

    Create Gold.java

---
+++ Gold.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+public class Gold{
+  public static String concate(Object o0,Object o1){
+    return o0+""+o1;
+  }
+  public static void main(String[] args){
+    String file = args[0];
+    interpeter
+  }
+}
