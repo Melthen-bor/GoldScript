8bc14f3a020682fcf7984799709fbc856de6160b
Melthen-bor <151883748+Melthen-bor@users.noreply.github.com>
2023-11-27 17:06:55 -0800

    Update goldException.java

--- core/com/gold/gold/goldException.java
+++ core/com/gold/gold/goldException.java
@@ -4,19 +4,19 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 public class goldExeception{
-  static String errormessage;
-  static String type;
-  public static String concate(Object o0,Object o1){
+  String errormessage;
+  String type;
+  public String concate(Object o0,Object o1){
     return o0+""+o1;
   }
-  public static goldExeception(String error,String errortype){
+  public goldExeception(String error,String errortype){
     errormessage = concate("\033[31m",concate(error,"\033[0m")));
     type = errortype;
   }
-  public static String gettype(){
+  public String gettype(){
     return type;
   }
-  public static String getmessage(){
+  public String getmessage(){
     return errormessage;
   }
 }
