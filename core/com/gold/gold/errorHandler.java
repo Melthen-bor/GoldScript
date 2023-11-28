38a4bd9b6b6ec5b072d69c574826e3e45f43e215
Melthen-bor <151883748+Melthen-bor@users.noreply.github.com>
2023-11-27 17:09:02 -0800

    Update errorHandler.java

--- core/com/gold/gold/errorHandler.java
+++ core/com/gold/gold/errorHandler.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList
 public class errorHandler{
   ArrayList<goldException> errors;
   int nexterror;
