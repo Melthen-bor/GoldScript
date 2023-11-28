a6e94bc87713a7d6683c0332992e29f8a262cfad
Melthen-bor <151883748+Melthen-bor@users.noreply.github.com>
2023-11-27 17:05:18 -0800

    Update file_reader.java

--- core/com/gold/gold/file_reader.java
+++ core/com/gold/gold/file_reader.java
@@ -4,12 +4,12 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 public class file_reader{
-  static File file;
-  public static file_reader(static String file_name){
+  File file;
+  public file_reader(static String file_name){
     static String fileName = file_name;
     file = new File(fileName);
   }
-  public static List read(){
+  public List read(){
     ArrayList<String> out = new ArrayList<String>();
     int a = 1;
     String temp;
@@ -25,7 +25,7 @@ public class file_reader{
     fileHandler.close();
     return out;
   }
-  public static void change(String file_name){
+  public void change(String file_name){
     file.close();
     file = new File(file_name);
   }
