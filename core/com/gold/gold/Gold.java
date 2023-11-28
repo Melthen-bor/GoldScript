bf9029f81234d95fefe2da65aab7ae19194889f1
Melthen-bor <151883748+Melthen-bor@users.noreply.github.com>
2023-11-27 17:06:22 -0800

    Update Gold.java

--- core/com/gold/gold/Gold.java
+++ core/com/gold/gold/Gold.java
@@ -4,10 +4,10 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 public class Gold{
-  public static String concate(Object o0,Object o1){
+  public String concate(Object o0,Object o1){
     return o0+""+o1;
   }
-  public static void main(String[] args){
+  public void main(String[] args){
     String file = args[0];
     String[] intargs = args[1].split("[split]");
     interpreter inter = new interpreter(intargs);
