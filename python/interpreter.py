bdd73ab160394618d596dbdab84a0694bef6790d
Melthen-bor <151883748+Melthen-bor@users.noreply.github.com>
2023-11-28 20:26:35 -0800

    Update interpreter.py

--- python/interpreter.py
+++ python/interpreter.py
@@ -3,12 +3,30 @@ import goldException
 import errorHandler
 class interpreter:
   custom=[]
-  def __init__(self,args):
+  def __init__(self,args=["Gold_Program"]):
     custom=args
   def run(self,filename):
+    ret=""
+    Ehandler=errorHandler();
     fileName=filename+".gold"
     file=open(fileName)
     contents=file.read()
     file.close()
     contents=contents.split("\n")
-    
+    a=1
+    b=0
+    tempint=0
+    tempint2=0
+    tempint3=0
+    tempstring=""
+    inloc=custom[0]
+    lastgolderror=""
+    tempinter=""
+    while a==1:
+      unparse=contents[b]
+      command=unparse.split(" ")
+      tempstring=command[0]
+      if command=="goif":
+        tempstring=command[1]
+        tempint=int(command[2])
+        
