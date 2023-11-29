fd5aeba867b511740d02faed50b7c13fddd76b61
Melthen-bor <151883748+Melthen-bor@users.noreply.github.com>
2023-11-28 19:54:51 -0800

    Update goldException.py

--- python/goldException.py
+++ python/goldException.py
@@ -1,1 +1,11 @@
-import
+#import
+class goldException():
+  message=""
+  type=""
+  def _init_(self,error,type):
+    self.message=error
+    self.type=type
+  def geterror(self):
+    return self.message
+  def gettype(self):
+    return self.type
