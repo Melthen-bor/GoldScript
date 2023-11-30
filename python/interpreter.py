b8980d897d72db209eadbcfd0a015762d30952f2
Melthen-bor <151883748+Melthen-bor@users.noreply.github.com>
2023-11-29 17:02:54 -0800

    Update interpreter.py

--- python/interpreter.py
+++ python/interpreter.py
@@ -1,6 +1,15 @@
 import datetime
-import goldException
 import errorHandler
+class goldException():
+  message=""
+  type=""
+  def _init_(self,error,type):
+    self.message="\033[31m"+error+"\033[0m"
+    self.type=type
+  def geterror(self):
+    return self.message
+  def gettype(self):
+    return self.type
 class interpreter:
   custom=[]
   def concate(self,s1,s2):
