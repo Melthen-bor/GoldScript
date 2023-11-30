9f50be7eeb18c8a8f0abdcc13e77722dd3c9980e
Melthen-bor <151883748+Melthen-bor@users.noreply.github.com>
2023-11-29 17:02:24 -0800

    Update interpreter.py

--- python/interpreter.py
+++ python/interpreter.py
@@ -3,11 +3,13 @@ import goldException
 import errorHandler
 class interpreter:
   custom=[]
+  def concate(self,s1,s2):
+    return s1+""+s2
   def __init__(self,args=["Gold_Program"]):
     custom=args
   def run(self,filename):
     ret=""
-    Ehandler=errorHandler();
+    Ehandler=errorHandler()
     fileName=filename+".gold"
     file=open(fileName)
     contents=file.read()
@@ -22,11 +24,39 @@ class interpreter:
     inloc=custom[0]
     lastgolderror=""
     tempinter=""
+    vars=[]
     while a==1:
       unparse=contents[b]
       command=unparse.split(" ")
       tempstring=command[0]
       if command=="goif":
-        tempstring=command[1]
-        tempint=int(command[2])
-        
+        try:
+          tempstring=command[1]
+          tempint=int(tempstring)
+          tempstring=command[2]
+          tempint2=int(vars[int(command[3])])
+          tempint3=int(vars[int(command[4])])
+          if tempstring=="==":
+            if tempint2==tempint3:
+              b=tempint
+          elif tempstring==">":
+            if tempint2>tempint3:
+              b=tempint
+          elif tempstring=="<":
+            if tempint2<tempint3:
+              b=tempint
+          elif tempstring==">=":
+            if tempint2>=tempint3:
+              b=tempint
+          elif tempstring=="<=":
+            if tempint2<=tempint3:
+              b=tempint
+          elif tempstring=="!=":
+            if tempint2!=tempint3:
+              b=tempint
+          else:
+            lastgolderror=goldException(self.concate(inloc,":Error[2]:Failed to parse command <goif>"),"Command_error")
+            Ehandler.adderror(lastgolderror)
+        except:
+          lastgolderror=goldException(self.concate(inloc,":Error[2]:Failed to parse command <goif>"),"Command_error")
+      elif
