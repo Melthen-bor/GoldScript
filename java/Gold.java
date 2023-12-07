f64ef08255e10330db46afa8ff853c4ed133f8cd
Melthen-bor <151883748+Melthen-bor@users.noreply.github.com>
2023-12-06 18:18:01 -0800

    Update Gold.java

--- java/Gold.java
+++ java/Gold.java
@@ -7,7 +7,7 @@ public class Gold{
   public String concate(Object o0,Object o1){
     return o0+""+o1;
   }
-  public void main(String[] args){
+  public static void main(String[] args){
     String file = args[0];
     String[] intargs = args[1].split("[split]");
     interpreter inter = new interpreter(intargs);
