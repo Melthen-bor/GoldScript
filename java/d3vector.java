bcfc027a076c74c0cffd8e6fda5f66a904e390c0
Melthen-bor <151883748+Melthen-bor@users.noreply.github.com>
2023-12-24 10:01:56 -0800

    Create d3vector.java
    Signed-off-by: Melthen-bor <151883748+Melthen-bor@users.noreply.github.com>

---
+++ java/d3vector.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Arrays;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptEngine;
+import java.lang.Math;
+import javax.vecmath.Vector3d;
+public class d3vector{
+  public String concate(object s0,object s1){
+    return s0+""+s1;
+  }
+  public d3vector(int[] vec){
+    this.a=vec[0];
+    this.b=vec[1];
+    this.c=vec[2];
+  }
+  public int[] to_list(){
+    vec=[]={"","",""};
+    vec[0]=this.a;
+    vec[1]=this.b;
+    vec[2]=this.c;
+  }
+  public void cross(int[] a,int[] b){
+    this.a=a[
+  }
+}
