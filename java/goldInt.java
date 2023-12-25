c670b0d8a764adefab77575b431f30fd4e52ac62
Melthen-bor <151883748+Melthen-bor@users.noreply.github.com>
2023-12-24 22:52:07 -0800

    Create goldInt.java
    Signed-off-by: Melthen-bor <151883748+Melthen-bor@users.noreply.github.com>

---
+++ java/goldInt.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Arrays;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptEngine;
+import java.lang.Math;
+import javax.vecmath.Vector3d;
+public static class goldInt extends goldObject{
+  Integer value;
+  public goldInteger(Integer val){
+    this.value=val;
+  }
+}
