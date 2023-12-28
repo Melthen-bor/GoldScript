dd67c175007b259f9e0b5cf8e670bd40dd5b006f
Melthen-bor <151883748+Melthen-bor@users.noreply.github.com>
2023-12-28 14:40:56 -0800

    Update interpreter.java
    Signed-off-by: Melthen-bor <151883748+Melthen-bor@users.noreply.github.com>

--- java/interpreter.java
+++ java/interpreter.java
@@ -14,19 +14,66 @@ import javax.script.ScriptEngine;
 import java.lang.Math;
 import javax.vecmath.Vector3d;
 public class interpreter{
-  static ArrayList<String> custom = ArrayList<String>();
+  //static ArrayList<String> custom = ArrayList<String>();
   static int line;
-  static ArrayList<String> vars = ArrayList<String>();
+  static ArrayList<goldObject> vars = ArrayList<goldObject>();
+  static errorHandler Ehandler = errorHandler();
+  static String inloc;
   public static String concate(Object o0,Object o1){
     return o0+""+o1;
   }
   public static interpreter(String[] args){
-    this.custom.addAll(Arrays.asList(args));
+    this.inloc=args[0];
   }
   public static void goif(String[] args){
-    int a = Integer.parseInt(args[0]);
-    String b = args[1];
-    Float c = Float.valueOf(vars.get(Integer.parseInt(args[2])));
-    Float d = Float.valueOf(vars.get(Integer.parseInt(args[3])));
+    try{
+      int a = Integer.parseInt(args[0]);
+      String b = args[1];
+      Float c = this.vars.get(Integer.parseInt(args[2])).getValue();
+      Float d = this.vars.get(Integer.parseInt(args[3])).getValue();
+      if (b.equals("==")){
+        if(c==d){
+          this.line=a;
+        }
+      } else if(b.equals(">")){
+        if(c>d){
+          this.line=a;
+        }
+      } else if(b.equals("<")){
+        if(c<d){
+          this.line=a;
+        }
+      } else if(b.equals(">=")){
+        if(c>=d){
+          this.line=a;
+        }
+      } else if(b.equals("<=")){
+        if(c<=d){
+          this.line=a;
+        }
+      } else if(b.equals("!")){
+        if(c!=d){
+          this.line=a;
+        }
+      } else{
+        Ehandler.adderror(new goldException(concate(inloc,":Error[2]:Failed to parse command <goif>"),"Command_error"));
+      }
+    } catch(Exception e){
+      Ehandler.adderror(new goldException(concate(inloc,":Error[2]:Failed to parse command <goif>"),"Command_error"));
+    }
+  }
+  public static void out(String[] args){
+    try{
+      a=args[0];
+      if(a.equals("Lorem_Ipsum")){
+        System.out.print("We Three Kings of Orient are,\nBearing gifts we traverse afar,\nField and fountain,\nMoor and mountain,\nFollowing yonder Star.");
+      } else if(a.equals("string")){
+        System.out.print(args[1]);
+      } else if(a.equals("variable")){
+        System.out.print(this.vars.get(Integer.parseInt(args[2])).getValue());
+      }
+    } catch(Exception e){
+      Ehandler.adderror(new goldException(concate(inloc,":Error[8]:Failed to parse command <out>"),"Command_error"))
+    }
   }
 }
