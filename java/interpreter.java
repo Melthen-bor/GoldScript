b97a660fb81a1897f18640dfcc2bca05a0a912f8
Melthen-bor <151883748+Melthen-bor@users.noreply.github.com>
2023-12-28 19:09:59 -0800

    Update interpreter.java
    Signed-off-by: Melthen-bor <151883748+Melthen-bor@users.noreply.github.com>

--- java/interpreter.java
+++ java/interpreter.java
@@ -19,13 +19,38 @@ public class interpreter{
   static ArrayList<goldObject> vars = ArrayList<goldObject>();
   static errorHandler Ehandler = errorHandler();
   static String inloc;
+  static String ret;
+  static String[] prevars;
+  static Scanner inputHandler = new Scanner(System.in);
+  static ScriptEngineManager mgr = new ScriptEngineManager();
+  static ScriptEngine js = mgr.getEngineByName("JavaScript");
+  public static String parseList(int[] a){
+    int b = 1;
+    int c = 1;
+    String d = String.valueOf(int[0]);
+    while(b==1){
+      try{
+        d=concate(d,concate("#",String.valueOf(int[c])));
+        c++;
+      } catch(Exception e){
+        b=0;
+      }
+    }
+  }
   public static String concate(Object o0,Object o1){
     return o0+""+o1;
   }
   public static interpreter(String[] args){
-    this.inloc=args[0];
+    this.inloc = args[0];
   }
-  public static void goif(String[] args){
+  public static void gocmd(String args){
+    try{
+      this.line = Integer.parseInt(args[0]);
+    } catch(Exception e){
+      this.Ehandler.adderror(new goldException(concate(this.inloc,":Error[1]:Failed to parse command <go>"),"Command_error"));
+    }
+  }
+  public static void goifcmd(String[] args){
     try{
       int a = Integer.parseInt(args[0]);
       String b = args[1];
@@ -56,24 +81,212 @@ public class interpreter{
           this.line=a;
         }
       } else{
-        Ehandler.adderror(new goldException(concate(inloc,":Error[2]:Failed to parse command <goif>"),"Command_error"));
+        this.Ehandler.adderror(new goldException(concate(this.inloc,":Error[2]:Failed to parse command <goif>"),"Command_error"));
       }
     } catch(Exception e){
-      Ehandler.adderror(new goldException(concate(inloc,":Error[2]:Failed to parse command <goif>"),"Command_error"));
+      this.Ehandler.adderror(new goldException(concate(this.inloc,":Error[2]:Failed to parse command <goif>"),"Command_error"));
     }
   }
-  public static void out(String[] args){
+  public static void throwcmd(String[] args){
+    String a;
+    String b;
     try{
-      a=args[0];
-      if(a.equals("Lorem_Ipsum")){
-        System.out.print("We Three Kings of Orient are,\nBearing gifts we traverse afar,\nField and fountain,\nMoor and mountain,\nFollowing yonder Star.");
-      } else if(a.equals("string")){
-        System.out.print(args[1]);
-      } else if(a.equals("variable")){
-        System.out.print(this.vars.get(Integer.parseInt(args[2])).getValue());
+      a = args[0];
+      try{
+        b = args[1];
+        if(b.equals("Command_error")||b.equals("System_error")){
+          b="Thrown_error";
+        }
+      } catch(Exception e){
+        b="Thrown_error";
       }
     } catch(Exception e){
-      Ehandler.adderror(new goldException(concate(inloc,":Error[8]:Failed to parse command <out>"),"Command_error"))
+      a="Error[?]:undefined error";
+      b="Thrown_error";
     }
+    this.Ehandler.adderror(new goldException(concate(this.inloc,concate(":",a)),b));
+  }
+  public static void importcmd(String[] args){
+    try{
+      Interpreter tempinter = new Interpreter({args[2]});
+      vars.set(Integer.parseInt(args[1]),tempinter.run(args[0],this.vars.get(Integer.parseInt(args[3])).getValue().split("#")));
+    } catch(Exception e){
+      this.Ehandler.adderror(new goldException(concate(this.inloc,":Error[3]:Failed to parse command <import>"),"Command_error"));
+    }
+  }
+  public static void returncmd(String[] args){
+    try{
+      String a = args[0];
+      if(a.equals("String")){
+        this.ret=args[1];
+      } else if(a.equals("Variable"){
+        this.ret=this.vars.get(Integer.parseInt(args[1])).asString();
+      }
+    } catch(Exception e){
+      this.Ehandler.adderror(new goldException(concate(this.inloc,":Error[4]:Failed to parse command <return>"),"Command_error"));
+    }
+  }
+  public static void assigncmd(String[] args){
+    try{
+      String a = args[0];
+      if(a.equals("String")){
+        this.vars.set(Integer.parseInt(args[1]),new goldString(args[2]));
+      } else if(a.equals("Prevar")){
+        a=args[1]
+        if(a.equals("String")){
+          this.vars.set(Integer.parseInt(args[2]),new goldString(this.prevars[Integer.parseInt(args[3])]));
+        } else if(a.equals("Number")){
+          this.vars.set(Integer.parseInt(args[2]),new goldNum(Float.valueFloat(this.prevars[Integer.parseInt(args[3])])));
+        }
+      } else if(a.equals("Number")){
+        this.vars.set(Integer.parseInt(args[1]),new goldNum(Float.valueFloat(args[3])));
+      } else if(a.equals("Lorem_Ipsum")){
+        this.vars.set(Integer.parseInt(args[1]),new goldString("We Three Kings of Orient are,\nBearing gifts we traverse afar,\nField and fountain,\nMoor and mountain,\nFollowing yonder Star."));
+      } else{
+        this.Ehandler.adderror(new goldException(concate(this.inloc,":Error[5]:Failed to parse command <assign>"),"Command_error"));
+      }
+    } catch(Exception e){
+      this.Ehandler.adderror(new goldException(concate(this.inloc,":Error[5]:Failed to parse command <assign>"),"Command_error"));
+    }
+  }
+  public static void timecmd(String[] args){
+    try{
+      this.vars.set(Integer.parseInt(args[0]),new goldString(LocalDateTime.now(DateTimeFormatter.ofPattern(args[1]))));
+    } catch(Exception e){
+      this.Ehandler.adderror(new goldException(concate(this.inloc,":Error[6]:Failed to parse command <time>"),"Command_error"));
+    }
+  }
+  public static void outcmd(String[] args){
+    try{
+      a = args[0];
+      if(a.equals("String")){
+        System.out.print(args[1]);
+      } else if(a.equals("Variable")){
+        System.out.print(this.vars.get(Integer.parseInt(args[1])).getValue());
+      } else if(a.equals("New")){
+        System.out.println();
+      } else if(a.equals("Return")){
+        System.out.print("\r");
+      } else{
+        this.Ehandler.adderror(new goldException(concate(this.inloc,":Error[7]:Failed to parse command <out>"),"Command_error"));
+      }
+    } catch(Exception e){
+      this.Ehandler.adderror(new goldException(concate(this.inloc,":Error[7]:Failed to parse command <out>"),"Command_error"));
+    }
+  }
+  public static void implementcmd(String[] args){
+    try{
+      this.vars.set(Integer.parseInt(args[0]),new goldString("0"));
+    } catch(Exception e){
+      this.Ehandler.adderror(new goldException(concate(this.inloc,":Error[8]:Failed to parse command <implement>"),"Command_error"));
+    }
+  }
+  public static void inputcmd(String[] args){
+    try{
+      this.vars.set(Integer.parseInt(args[0]),new goldString(this.inputHandler.nextLine()));
+    } catch(Exception e){
+      this.Ehandler.adderror(new goldException(concate(this.inloc,":Error[9]:Failed to parse command <input>"),"Command_error"));
+    }
+  }
+  public static void operationcmd(String[] args){
+    try{
+      a = args[0];
+      if(a.equals("+")){
+        this.vars.set(Integer.parseInt(args[1]),new goldNum(this.vars.get(Integer.parseInt(args[2])).getValue()+this.vars.get(Integer.parseInt(args[3])).getValue()));
+      } else if(a.equals("-")){
+        this.vars.set(Integer.parseInt(args[1]),new goldNum(this.vars.get(Integer.parseInt(args[2])).getValue()-this.vars.get(Integer.parseInt(args[3])).getValue()));
+      } else if(a.equals("*")){
+        this.vars.set(Integer.parseInt(args[1]),new goldNum(this.vars.get(Integer.parseInt(args[2])).getValue()*this.vars.get(Integer.parseInt(args[3])).getValue()));
+      } else if(a.equals(":")){
+        this.vars.set(Integer.parseInt(args[1]),new goldNum(this.vars.get(Integer.parseInt(args[2])).getValue()/this.vars.get(Integer.parseInt(args[3])).getValue()));
+      } else if(a.equals("**")){
+        this.vars.set(Integer.parseInt(args[1]),new goldNum(Math.pow(this.vars.get(Integer.parseInt(args[2])).getValue(),this.vars.get(Integer.parseInt(args[3])).getValue())));
+      } else if(a.equals("::")){
+        this.vars.set(Integer.parseInt(args[1]),new goldNum(Math.pow(this.vars.get(Integer.parseInt(args[2])).getValue(),(1f/this.vars.get(Integer.parseInt(args[3])).getValue()))));
+      } else if(a.equals("%")){
+        this.vars.set(Integer.parseInt(args[1]),new goldNum(this.vars.get(Integer.parseInt(args[2])).getValue()%this.vars.get(Integer.parseInt(args[3])).getValue()));
+      } else if(a.equals("$")){
+        this.vars.set(Integer.parseInt(args[1]),new goldNum((this.vars.get(Integer.parseInt(args[2])).getValue()%2f==0f)?0f:1f));
+      } else if(a.equals("&&")){
+        this.vars.set(Integer.parseInt(args[1]),new goldString(concate(this.vars.get(Integer.parseInt(args[2])).getValue(),this.vars.get(Integer.parseInt(args[3])).getValue())));
+      } else if(a.equals("&")){
+        this.vars.set(Integer.parseInt(args[1]),new goldNum(this.vars.get(Integer.parseInt(args[2])).getValue()&this.vars.get(Integer.parseInt(args[3])).getValue()));
+      } else if(a.equals("|")){
+        this.vars.set(Integer.parseInt(args[1]),new goldNum(this.vars.get(Integer.parseInt(args[2])).getValue()|this.vars.get(Integer.parseInt(args[3])).getValue()));
+      } else if(a.equals("^")){
+        this.vars.set(Integer.parseInt(args[1]),new goldNum(this.vars.get(Integer.parseInt(args[2])).getValue()^this.vars.get(Integer.parseInt(args[3])).getValue()));
+      } else if(a.equals("~")){
+        this.vars.set(Integer.parseInt(args[1]),new goldNum(~this.vars.get(Integer.parseInt(args[2])).getValue()));
+      } else if(a.equals(">>")){
+        this.vars.set(Integer.parseInt(args[1]),new goldNum(this.vars.get(Integer.parseInt(args[2])).getValue()>>));
+      } else if(a.equals(">>>")){
+        this.vars.set(Integer.parseInt(args[1]),new goldNum(this.vars.get(Integer.parseInt(args[2])).getValue()>>>));
+      } else if(a.equals("<<")){
+        this.vars.set(Integer.parseInt(args[1]),new goldNum(this.vars.get(Integer.parseInt(args[2])).getValue()<<));
+      } else if(a.equals("E")){
+        this.vars.set(Integer.parseInt(args[1]),new goldString(this.js.eval(this.vars.get(Integer.parseInt(args[2])).getValue())));
+      } else if(a.equals("N")){
+        this.vars.set(Integer.parseInt(args[1]),new goldNum(Float.valueFloat(this.vars.get(Integer.parseInt(args[2])))));
+      } else if(a.equals("X")){
+        this.vars.set(Integer.parseInt(args[1]),new goldString(parseList((new d3vector()).cross((new d3vector(this.vars.get(Integer.parseInt(args[2])).getValue().split("#")).to_list()),(new d3vector(this.vars.get(Integer.parseInt(args[3])).getValue().split("#")).to_list())).to_list())));
+      } else{
+        this.Ehandler.adderror(new goldException(concate(this.inloc,":Error[10]:Failed to parse command <operation>"),"Command_error"));
+      }
+    } catch(Exception e){
+      this.Ehandler.adderror(new goldException(concate(this.inloc,":Error[10]:Failed to parse command <operation>"),"Command_error"));
+    }
+  }
+  public static String run(String filename,String[] prevars){
+    try{
+      file_reader fileHandler = new file_reader(concate(filename,".gold"));
+      this.prevars = prevars;
+      ArrayList<String> contents = new ArrayList<String>();
+      contents.addAll(fileHandler.read());
+      int a = 1;
+      int b = 0;
+      String tempstring;
+      ArrayList<String> command = new ArrayList<String>();
+      String[] templist;
+      while(a==1){
+        try{
+          command.addAll(Arrays.asList(content.get(b).split(" ")));
+          tempstring = command.get(0);
+          command.remove(0);
+          if(tempstring.equals("go")){
+            this.gocmd(command.toArray());
+          } else if(tempstring.equals("goif")){
+            this.goifcmd(command.toArray());
+          } else if(tempstring.equals("throw")){
+            this.throwcmd(command.toArray());
+          } else if(tempstring.equals("import")){
+            this.importcmd(command.toArray());
+          } else if(tempstring.equals("return")){
+            this.returncmd(command.toArray());
+          } else if(tempstring.equals("assign")){
+            this.returncmd(command.toArray());
+          } else if(tempstring.equals("time")){
+            this.returncmd(command.toArray());
+          } else if(tempstring.equals("out")){
+            this.outcmd(command.toArray());
+          } else if(tempstring.equals("implement")){
+            this.implementcmd(command.toArray());
+          } else if(tempstring.equals("operation")){
+            this.operationcmd(command.toArray());
+          } else if(tempstring.equals("comment")){
+            System.out.print();
+          } else{
+            this.Ehandler.adderror(new goldException(concate(this.inloc,":Error[0]:Unknown command"),"Command_error"));
+          }
+          command.clear();
+          b += 1;
+        } catch{
+          a = 0;
+        }
+      }
+    } catch(Exception e){
+      this.Ehandler.adderror(new goldException("Gold_Interpreter:Error[0]:Critical failure","System_error"));
+    }
+    this.Ehandler.rethrow();
+    return this.ret;
   }
 }
