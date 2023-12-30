76634fc934069c60e0a19523d3f1175876658ea6
Melthen-bor <151883748+Melthen-bor@users.noreply.github.com>
2023-12-30 12:50:20 -0800

    Create compiler.java
    Signed-off-by: Melthen-bor <151883748+Melthen-bor@users.noreply.github.com>

---
+++ compiler/compiler.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Arrays;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptEngine;
+import java.lang.Math;
+import javax.vecmath.Vector3d;
+public class compiler{
+  private final String TAB = "^I";
+  private static char Look;
+  private static int line;
+  public static String concate(Object s0,Object s1){
+    return s0+""+s1;
+  }
+  public static compiler(){
+    this.line = 0;
+  }
+  public static compile(String fileName){
+    try{
+      file_reader source = new file_reader(fileName);
+      ArrayList<String> content = new ArrayList<String>();
+      content.addAll(source.read());
+      int a = 1;
+      int b = 0;
+      ArrayList<String> command = new ArrayList<String>();
+      String tempstring;
+      while(a==1){
+        try{
+          command.addAll((content.get(b)).split(" "));
+          tempstring = command.get(0);
+          if(tempstring.equals("
+        } catch(Exception e){
+          a = 0;
+        }
+      }
+    } catch(Exception e){
+      System.out.println("\033[31mFailed to Compile\033[0m");
+    }
+  }
+}
