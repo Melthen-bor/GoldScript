bf172b48d4b34211e6171bf5e87ca277a9f08628
Melthen-bor <151883748+Melthen-bor@users.noreply.github.com>
2023-12-30 13:56:21 -0800

    Update compiler.java
    Signed-off-by: Melthen-bor <151883748+Melthen-bor@users.noreply.github.com>

--- compiler/compiler.java
+++ compiler/compiler.java
@@ -23,7 +23,20 @@ public class compiler{
   public static compiler(){
     this.line = 0;
   }
-  public static compile(String fileName){
+  public static String parseList(String[] list,String l){
+    int a = 1;
+    int b = 1;
+    String c = list[0];
+    while(a==1){
+      try{
+        c = concate(c,concate(l,list[1]));
+      } catch(Exception e){
+        a = 0;
+      }
+    }
+    return c
+  }
+  public static void compile(String fileName){
     try{
       file_reader source = new file_reader(concate(fileName,".gold"));
       ArrayList<String> content = new ArrayList<String>();
@@ -33,18 +46,29 @@ public class compiler{
       ArrayList<String> command = new ArrayList<String>();
       String tempstring;
       String replace = "";
+      file_reader include;
       while(a==1){
         try{
           command.addAll((content.get(b)).split(" "));
           tempstring = command.get(0);
           if(tempstring.equals("jump")){
             replace = concate(replace,concate("JMP rel32 ",command.get(1)));
-          } else(tempstring.equals("jumpif")){
+          } else if(tempstring.equals("jumpif")){
             tempstring = command.get(2);
             if(tempstring.equals("==")){
-              replace = concate(replace,concate(concate(concate(concate("IF ",command.get(3)),concate("=",command.get(4))),"\n"),concate(concate("  JMP rel32 ",concate(command.get(1),"\n"),"FI"))));
+              replace = concate(replace,concate(concate(concate(concate("IF ",command.get(3)),concate(" = ",command.get(4))),"\n"),concate(concate("  JMP rel32 ",concate(command.get(1),"\n"),"FI"))));
+            } else if(tempstring.equals(">")){
+              replace = concate(replace,concate(concate(concate(concate("IF ",command.get(3)),concate(" > ",command.get(4))),"\n"),concate(concate("  JMP rel32 ",concate(command.get(1),"\n"),"FI"))));
+            } else if(tempstring.equals("<")){
+              replace = concate(replace,concate(concate(concate(concate("IF ",command.get(3)),concate(" < ",command.get(4))),"\n"),concate(concate("  JMP rel32 ",concate(command.get(1),"\n"),"FI"))));
             }
+          } else if(tempstring.equals("import")){
+            include = new file_reader(command.get(1));
+            replace = concate(replace,parseList(include.read(),"\n"));
+          } else if(tempstring.equals("assign")){
+            
           }
+          replace = concate(replace,"\n");
         } catch(Exception e){
           a = 0;
         }
