ff36675c1d05532030b60868f13d9ca6976d13e9
Melthen-bor <151883748+Melthen-bor@users.noreply.github.com>
2023-12-30 11:01:58 -0800

    Update interpreter.cpp
    Signed-off-by: Melthen-bor <151883748+Melthen-bor@users.noreply.github.com>

--- cpp/interpreter.cpp
+++ cpp/interpreter.cpp
@@ -1,7 +1,28 @@
 #include <iostream>
 #include <thread>
 #include <string>
+#include <list>
 using namespace std;
-static int go(string[] args){
+class goldObject{
+  String val;
+  public goldObject(String value){
+    this.val = value;
+  }
+  public getValue(){
+    return this.val;
+  }
+  public setValue(String value){
+    this.val = value;
+  }
+}
+class goldString: private goldObject{
+  public goldString(String value){
+  }
+}
+class interpreter{
+  int line;
   
+  public interpreter(){
+    line = 0;
+  }
 }
