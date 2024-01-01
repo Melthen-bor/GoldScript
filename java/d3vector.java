9ff8b1e6c0a5d6d18c0c4aa337c4e738e70f0f16
Melthen-bor <151883748+Melthen-bor@users.noreply.github.com>
2024-01-01 12:39:39 -0800

    Update d3vector.java
    Signed-off-by: Melthen-bor <151883748+Melthen-bor@users.noreply.github.com>

--- java/d3vector.java
+++ java/d3vector.java
@@ -17,28 +17,51 @@ public class d3vector{
   int a;
   int b;
   int c;
-  public String concate(object s0,object s1){
+  public static String concate(object s0,object s1){
     return s0+""+s1;
   }
-  public d3vector(String[] vec){
+  public static d3vector(String[] vec){
     this.a=Integer.parseInt(vec[0]);
     this.b=Integer.parseInt(vec[1]);
     this.c=Integer.parseInt(vec[2]);
   }
-  public int[] to_list(){
+  public static int[] to_list(){
     vec=[]={"","",""};
     vec[0]=this.a;
     vec[1]=this.b;
     vec[2]=this.c;
   }
-  public void cross(int[] a,int[] b){
+  public static void cross(int[] a,int[] b){
     this.a=a[1]*b[2]-a[2]*b[1];
     this.b=a[2]*b[0]-a[0]*b[2];
     this.c=a[0]*b[1]-a[1]*b[0];
   }
-  public void dot(int[] a,int[] b){
+  public static void cross(int[] a){
+    d=this.a;
+    e=this.b;
+    f=this.c;
+    this.a=e*a[2]-f*a[1];
+    this.b=f*a[0]-d*a[2];
+    this.c=d*a[1]-e*a[0];
+  }
+  public static int dot(int[] a){
+    d=a[0]*this.a;
+    d+=a[1]*this.b;
+    d+=a[2]*this.c;
+    return d
+  }
+  public static int perpdot(int[] a){
+    d=a[0]*this.b-a[1]*this.a;
+    return d
+  }
+  public static void hadamard(int[] a,int[] b){
     this.a=a[0]*b[0];
     this.b=a[1]*b[1];
-    this.c=a[2]*b[2];
+    this.c=a[2]*b[2]:
+  }
+  public static void hadamard(int[] a){
+    this.a=this.a*a[0];
+    this.b=this.b*a[1];
+    this.c=this.c*a[2];
   }
 }
