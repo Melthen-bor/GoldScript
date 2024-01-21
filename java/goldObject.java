de9b2bbdf7290fc993841bb72b793bd634227035
Melthen-bor <151883748+Melthen-bor@users.noreply.github.com>
2024-01-20 18:47:54 -0800

    Create goldObject.java
    Signed-off-by: Melthen-bor <151883748+Melthen-bor@users.noreply.github.com>

---
+++ java/goldObject.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Arrays;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptEngine;
+import java.lang.Math;
+public class goldObject{
+  boolean value;
+  public goldObject(){
+    this.value=0;
+  }
+  public static boolean getvalue(){
+    return this.value;
+  }
+}
