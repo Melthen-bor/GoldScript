3883a94112e3384b95e2be426d54140f9ce6d526
Malthein Bor <151883748+Melthen-bor@users.noreply.github.com>
2024-03-17 14:32:22 -0700

    Update goldObject.java
    Signed-off-by: Malthein Bor <151883748+Melthen-bor@users.noreply.github.com>

--- java/goldObject.java
+++ java/goldObject.java
@@ -1,3 +1,8 @@
+/*This file is licensed under The OPLL v1.5
+and is from the GoldScript repo
+hosted by Melthen-bor
+contains commitments by:
+Melthen-bor*/
 public class goldObject{
   goldObject(){
     System.out.print();
