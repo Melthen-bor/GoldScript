31f4687f58e35b62fcf413181c354528d37a5e54
Malthein Bor <151883748+Melthen-bor@users.noreply.github.com>
2024-03-17 14:36:42 -0700

    Update goldByte.java
    Signed-off-by: Malthein Bor <151883748+Melthen-bor@users.noreply.github.com>

--- java/goldByte.java
+++ java/goldByte.java
@@ -18,74 +18,10 @@ import javax.script.ScriptEngineManager;
 import javax.script.ScriptEngine;
 import java.lang.Math;
 import javax.vecmath.Vector3d;
-public class goldByte{
-  ArrayList<goldBit> value=new ArrayList<goldBit>();
+public class goldByte extends goldObject{
+  goldNibble nib0;
+  goldNibble nib1;
   public goldByte(){
-    int a=0;
-    while(a<0){
-      this.value.add(new goldBit);
-      a++;
-    }
-  }
-  public static int btd8(String s0){
-    int a=0;
-    String[] b=s0.split("2");
-    int c=0;
-    int d=64;
-    while(c<7){
-      if((b[c]).equals("1")){
-        a+d;
-      }
-      c++;
-    }
-    return a;
-  }
-  public static void not(){
-    int a=0;
-    while(a<8){
-      (this.value.get(b)).not();
-    }
-  }
-  public static String dtb8(int i0){
-    String a="";
-    int b=1;
-    int c=64;
-    int d;
-    if(i0<0){
-      a=tos(a,"12");
-    }
-    while(b<7){
-      d=i0-c;
-      if(d<0){
-        a=tos(a,"02");
-      } else{
-        a=tos(a,"12");
-        i0-c;
-      }
-      c/=2;
-      b++;
-    }
-    if(this.value.get(0)){
-      b=0-1;
-      c=b-a;
-      a=c;
-    }
-    return a;
-  }
-  public static int getvalue(){
-    String a="";
-    int b=0;
-    while(b<8){
-      if(this.value.get(b)){
-        a=tos(a,"12");
-      } else{
-        a=tos(a,"02");
-      }
-      b++;
-    }
-    return btd8(a);
-  }
-  public static goldByte __add__(goldByte byte0){
-    
+    super();
   }
 }
