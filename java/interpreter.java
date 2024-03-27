d096517cd99b2cc45acc5b4e9efb0f6803e85fd5
Malthein Bor <151883748+Melthen-bor@users.noreply.github.com>
2024-03-26 17:57:16 -0700

    Delete java/interpreter.java
    Signed-off-by: Malthein Bor <151883748+Melthen-bor@users.noreply.github.com>

--- java/interpreter.java
+++
@@ -1,9 +0,0 @@
-import java.util.ArrayList;
-public class interpreter{
-  private int line;
-  private ArrayList<String> vars;
-  public interpreter(){
-    this.line=0;
-    this.vars=new ArrayList<String>();
-  }
-  public
