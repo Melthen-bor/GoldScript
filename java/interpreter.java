1cd8eae8f4266049475640783bd66ee144cb1578
Malthein Bor <151883748+Melthen-bor@users.noreply.github.com>
2024-03-26 17:38:32 -0700

    Create interpreter.java
    Signed-off-by: Malthein Bor <151883748+Melthen-bor@users.noreply.github.com>

---
+++ java/interpreter.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+public class interpreter{
+  private int line;
+  private ArrayList<String> vars;
+  public interpreter(){
+    this.line=0;
+    this.vars=new ArrayList<String>();
+  }
+  public
